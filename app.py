bc648b6fe0c635e17847444f87d80db55c088f92
CHOUMMANIVONGDimitri <dimitrichoummanivong@gmail.com>
2023-09-13 16:39:47 +0200

    🔥 move mongo connection

--- app.py
+++ app.py
@@ -1,38 +1,16 @@
 import os
 from fastapi import FastAPI
 import uvicorn
-import certifi
 from dotenv import load_dotenv
-from pymongo.mongo_client import MongoClient
+from config.db import *
 from controllers.books import router as book_router
+from controllers.users import router as user_router
 
 load_dotenv()
 app = FastAPI()
 
-MONGODB_URI = os.getenv("MONGODB_URI")
-DATABASE = os.getenv("DATABASE")
 PORT = os.getenv("PORT")
 HOST = os.getenv("HOST")
-
-
-client = MongoClient(MONGODB_URI, tlsCAFile=certifi.where())
-
-try:
-    client.admin.command('ping')
-    print("Successfully connected to MongoDB!")
-
-    # Check if the database exists
-    if DATABASE not in client.list_database_names():
-        app.database = client[DATABASE]
-        # Create a default collection to trigger database creation
-        app.database.create_collection("users")
-        print(f"Database '{DATABASE}' created.")
-    else:
-        app.database = client[DATABASE]
-        print(f"Connected to '{DATABASE}' database...")
-
-except Exception as e:
-    print(e)
 
 
 @app.get("/")
@@ -40,6 +18,7 @@ async def get_welcome_message():
     return "Welcome to the API"
 
 app.include_router(book_router, tags=["books"], prefix="/book")
+app.include_router(user_router, tags=["users"], prefix="/users")
 
 
 if __name__ == "__main__":
